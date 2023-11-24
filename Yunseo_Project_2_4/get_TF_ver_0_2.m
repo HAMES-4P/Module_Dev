18ae7201c33e7df1b798a01ae83be289a207ae91
Yunseo37 <94425657+Yunseo37@users.noreply.github.com>
2023-11-24 17:04:42 +0900

    Add files via upload
    get step response for getting tf,
    get_tf, bode plot, tf re-check, get gains, step response plot to see overshoot and rise time

--- Yunseo_Project_2_4/get_TF_ver_0_2.m
+++ Yunseo_Project_2_4/get_TF_ver_0_2.m
@@ -1,15 +1,13 @@
-y2 = output_encoder(:, 2); % 첫 번째 열 전체를 가져오도록 수정
+y2 = output_encoder(:, 2); 
 u2 = input_step(:, 2);
 
-%u2(1, 1:2000) = 0;
+%step response 측정 모델의 설정대로 15초 기준
 y2(1:15000, 1) = y2(15000, 1);
 y2(:, 1) = y2(:, 1) - y2(15000, 1);
 
 t = (0:length(u2)-1) * 0.001;
 
-% tfestimate 함수를 사용하여 주파수 응답 추정
 Ts = 0.001; % 샘플링 주파수
-%[tf_estimate, f] = tfestimate(u2, y2, [], [], [], Fs);
 data = iddata(y2, u2, Ts);
 
 % tfest 함수를 사용하여 전달 함수 추정
@@ -37,18 +35,19 @@ t_plot = 0:0.01:5; % 시간 범위
 
 % y2와 y_estimate를 함께 플로팅
 figure;
-subplot(2,1,1);
+
 plot(t, y2, '-b', 'LineWidth', 2);
-xlabel('Time');
-ylabel('y2');
-title('Actual Output (y2)');
+hold on;
+plot(t, y_estimate, '--r', 'LineWidth', 2);
+hold off;
 
-subplot(2,1,2);
-plot(t, y_estimate, '-r', 'LineWidth', 2);
 xlabel('Time');
-ylabel('y\_estimate');
-title('Estimated Output (y\_estimate)');
+ylabel('y');
+title('Actual Output (y2) vs Estimated Output (y\_estimate)');
 
+legend('Actual Output (y2)', 'Estimated Output (y\_estimate)');
+
+% controller와 system의 step response 출력 => overshoot, rise time 보기 가능
 figure;
 plot(t_plot, y_plot);
 title('Step Response of the Closed-loop System');
